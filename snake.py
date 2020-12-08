785fac7106107805010ded1e3bb34cb0fcb544cf
nolngo <nolanngo@protonmail.com>
2020-12-08 05:10:20 -0800

    finished backend and functional methods

--- snake.py
+++ snake.py
@@ -16,7 +16,7 @@ SB_TEXT = 1
 SB_BACKGROUND = 8
 
 #### Game Over Colors ####
-GAMEOVER_TEXT = ["GAME OVER", "PLAY AGAIN?", "PRESS Y OR N"]
+GAMEOVER_TEXT = ["GAME OVER", "PLAY AGAIN?", "Q TO QUIT | R TO RETRY"]
 GAMEOVER_TEXT_COLOR = 0
 GAMEOVER_COLOR = 7
 
@@ -33,7 +33,7 @@ LEFT = Point(-1, 0)
 RIGHT = Point(1, 0)
 INITIAL = Point(5, 5)       ###The starting point of the game
 
-#### Classes ####
+########## Classes ##########
 
 class Snake: 
     def __init__(self):
@@ -45,17 +45,13 @@ class Snake:
         self.snake = deque()
         self.direction = RIGHT
         self.score = 0
-        self.death = False
+        self.dead = False
         self.spawn_food()
-         
-
-
-
+        self.snake.append(START)
 
 
     def update(self):
         pass
-
 
 
 
@@ -78,14 +74,33 @@ class Snake:
         snake_pixels = set(self.snake)
         self.food = self.snake[0]
         while self.food in snake_pixels:
-            x = randint(0, WIDTH - 1)            ### The '-1' is needed because the max width value would appear off screen
-            y = randint(0, HEIGHT - 1)
+            x = randint(0, WIDTH - 1)               ### The '-1' is needed because the max width value would appear off screen
+            y = randint(SB_HEIGHT + 1, HEIGHT - 1)
 
     def eat(self):
-
+        if self.snake[0] == self.food:              ### This line checks if the position of the head of the snake equals the food.
+            self.score += 1                         ### If the position of the head and the food matches, the snake will eat it and gain a point.
+            self.snake.append(self.popped_point)    ### The snake gets longer through this append to deque process. Making it easier to mess up.
+            self.spawn_food()                       ### When the food is eaten, a new one will spawn.
 
     def grow_snake(self):
-        head = self.snake[0]
-        new_head = Point(head.x + self.direction.x, head.y + self.direction.y)
+        old_head = self.snake[0]                                                                ### Identifies snake[0] as the head
+        new_head = Point(old_head.x + self.direction.x, old_head.y + self.direction.y)              
         self.snake.appendleft(new_head)
         self.popped_point = self.snake.pop()
+
+    def is_dead(self):
+        head = self.snake[0]
+        if head.x < 0 or head.y <= SB_HEIGHT or head.x > WIDTH or head.y >= HEIGHT:
+            self.game_over()
+        if len(self.snake) != len(set(self.snake)):
+            self.game_over()
+    
+    def game_over(self):
+        self.dead = True
+
+        pyxel.stop()
+
+########## Rendering ###########
+
+
