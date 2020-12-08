1fe99720ae8d1253da940052d93b25c054c98226
nolngo <nolanngo@protonmail.com>
2020-12-08 03:33:03 -0800

    added control buttons class

--- snake.py
+++ snake.py
@@ -5,22 +5,29 @@ import pyxel
 
 Point = namedtuple("point", ["x", "y"])
 
-#### Global Constants ####
+########## Global Constants ##########
+
+#### Main Screen Game Colors ####
 BACKGROUND_COLOR = 0
 BODY_COLOR = 7
 HEAD_COLOR = 3
 APPLE_COLOR = 8
-GAMEOVER_COLOR = 7
 
+#### Game Over Colors ####
 GAMEOVER_TEXT = ["GAME OVER", "PLAY AGAIN?", "PRESS Y OR N"]
 GAMEOVER_TEXT_COLOR = 0
+GAMEOVER_COLOR = 7
 
+#### Dimensions of Game Window ####
 WIDTH = 40
 HEIGHT = 50
 
-
-
-
+#### Define Nav Directions ####
+UP = Point(0, -1)
+DOWN = Point(0, 1)
+LEFT = Point(-1, 0)
+RIGHT = Point(1, 0)
+INITIAL = Point(5, 5)       ###The starting point of the game
 
 #### Classes ####
 
@@ -30,3 +37,25 @@ class Snake:
     
     def start(self):
         self.direction = RIGHT
+
+
+
+
+
+class update(self):
+    pass
+
+class controls(self):
+if pyxel.btn(pyxel.KEY_UP)
+    if self.direction is not DOWN:      ### In Snake, you cannot flip 180 degrees onto your own body, which is why we need the "is not" statement
+        self.direction = UP
+if pyxel.btn(pyxel.KEY_DOWN)
+    if self.direction is not UP:
+        self.direction = DOWN
+if pyxel.btn(pyxel.KEY_LEFT)
+    if self.direction is not RIGHT:
+        self.direction = LEFT
+if pyxel.btn(pyxel.KEY_RIGHT)
+    if self.direction is not LEFT:
+        self.direction = RIGHT
+
